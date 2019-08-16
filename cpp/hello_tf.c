fefe665aebb8a761c273c24d045474be3a4e72f4
Leo Pekelis <leo@opendoor.com>
2019-08-15 17:48:50 -0700

    remove example cpp files

--- cpp/hello_tf.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <tensorflow/c/c_api.h>
-#include <nlohmann/json.hpp>
-
-int main() {
-  printf("Hello from TensorFlow C library version %s\n", TF_Version());
-  return 0;
-}
